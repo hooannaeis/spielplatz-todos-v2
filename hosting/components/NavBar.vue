0b0a063f38cc6d0940a9879f2d0d2bd851fa0721
hooannaeis <hanneskuhl@live.de>
2022-01-30 15:36:24 +0100

    navbar introduced

---
+++ hosting/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="flex flex-row gap-5">
+    <li class="rounded-b-lg p-4 bg-gray-900 text-gray-50 shadow-lg">
+      <nuxt-link to="/"> ✔️ </nuxt-link>
+    </li>
+    <li class="rounded-b-lg p-4 bg-gray-900 text-gray-50 shadow-lg">
+      <nuxt-link :to="recipePath"> 📓 </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      recipePath: "/recipes"
+    }
+  }
+}
+</script>
+
+<style></style>
